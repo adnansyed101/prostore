1c256a4d905b90e64210807b425bc7541226028c
adnan <sirgeant10@gmail.com>
2025-04-29 14:44:09 +0600

    add metadata constant file.

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_NAME || "A modern ecom store built with the latest next js";
+export const SERVERL_URL = process.env.NEXT_PUBLIC_SErVER_URL || "http://localhost:300"
