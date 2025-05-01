02d113306dd88c113acd0e3e498051ab324ac525
adnan <sirgeant10@gmail.com>
2025-05-01 17:31:27 +0600

    Create product price component to show price in the product card component.

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) => {
+  // Ensure two decimal Value
+  const stringValue = value.toFixed(2);
+  //   Get the int/float
+  const [intValue, floatValue] = stringValue.split(".");
+
+  return (
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">$</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
