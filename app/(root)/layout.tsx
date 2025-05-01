3215fabe65c8ef77ca6c0a1e1d9f4ab97694e372
adnan <sirgeant10@gmail.com>
2025-05-01 15:56:24 +0600

    Add header and footer to layout.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
-export default function RootLayout({
+import Footer from "@/components/Footer";
+import Header from "@/components/shared/header";
+
+function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <div className="flex h-screen flex-col">
+      <Header />
       <main className="flex-1 wrapper">{children}</main>
+      <Footer />
     </div>
   );
 }
+
+export default RootLayout;
