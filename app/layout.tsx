a32cdcc26a49a2d74734e3459da33b4edaf6b117
adnan <sirgeant10@gmail.com>
2025-05-01 16:40:07 +0600

    Add hydration suppressor.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
+      <body
+        className={`${inter.className} antialiased`}
+        suppressHydrationWarning
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
