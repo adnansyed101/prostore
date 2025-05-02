106db1c8becd33f1d03b5e10807bb1d1da9cd208
Adnan <sirgeant10@gmail.com>
2025-05-02 17:43:19 +0600

    Add constant to limit latest product.

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,9 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_NAME || "A modern ecom store built with the latest next js";
-export const SERVERL_URL = process.env.NEXT_PUBLIC_SErVER_URL || "http://localhost:300"
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_NAME ||
+  "A modern ecom store built with the latest next js";
+export const SERVERL_URL =
+  process.env.NEXT_PUBLIC_SErVER_URL || "http://localhost:300";
+
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
