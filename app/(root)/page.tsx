0891af1a9a23c4b5baefb8689e1e904e77a8bfef
Adnan <sirgeant10@gmail.com>
2025-05-02 17:42:20 +0600

    Fetch data from server.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,15 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants";
 
-const Homepage = () => {
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <>
       <ProductList
-        data={sampleData.products}
+        data={latestProducts}
         title="Newest Arriavals"
-        limit={4}
+        limit={LATEST_PRODUCTS_LIMIT}
       />
     </>
   );
