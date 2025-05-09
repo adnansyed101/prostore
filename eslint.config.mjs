7fa9f12b0c0a77d2c57c194ed5b1c4754c65a63d
Adnan <sirgeant10@gmail.com>
2025-05-09 17:18:49 +0600

    Remove eslint for prisma generated errors.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    overrides: [
+      {
+        files: ["/prisma/migrations/*", "/lib/generated/*"],
+      },
+    ],
+  }),
 ];
 
 export default eslintConfig;
