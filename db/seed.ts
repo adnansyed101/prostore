8c3e2d99e45ff1f4497e936b41820eb0e11aae72
Adnan <sirgeant10@gmail.com>
2025-05-10 16:54:45 +0600

    Add user data to db.

--- db/seed.ts
+++ db/seed.ts
@@ -5,8 +5,15 @@ async function main() {
   const prisma = new PrismaClient();
   // Delete all existing products
   await prisma.product.deleteMany();
+  // Delete all existing users
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products });
+  
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully.");
 }
