38f2526f3bd132456f54d87f0041c7b6fa2ba152
Adnan <sirgeant10@gmail.com>
2025-05-11 14:54:37 +0600

    Remove override because not working.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.config({
-    overrides: [
-      {
-        files: ["/prisma/migrations/*", "/lib/generated/*"],
-      },
-    ],
-  }),
 ];
 
 export default eslintConfig;
