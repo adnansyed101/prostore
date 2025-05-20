858d6854e373910909ef7607b5434921a4a9e659
adnansyed101 <sirgeant10@gmail.com>
2025-05-20 14:01:19 +0600

    Create cart types.

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
-import { insertProductSchema } from "@/lib/validators";
+import {
+  insertProductSchema,
+  insertCartSchema,
+  cartItemSchema,
+} from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type cart = z.infer<typeof insertCartSchema>;
+
+export type cartitem = z.infer<typeof cartItemSchema>;
