2b549916e5d0cc45d31d6f3c3ed3df6e5d63bfe6
adnan <sirgeant10@gmail.com>
2025-05-22 16:46:57 +0600

    Fix variable names

--- lib/validators.ts
+++ lib/validators.ts
@@ -55,8 +55,8 @@ export const cartItemSchema = z.object({
 
 export const insertCartSchema = z.object({
   items: z.array(cartItemSchema),
-  itemsPice: currency,
-  totatlPrice: currency,
+  itemsPrice: currency,
+  totalPrice: currency,
   shippingPrice: currency,
   taxPrice: currency,
   sessionCartId: z.string().min(1, "Session cart id is required"),
