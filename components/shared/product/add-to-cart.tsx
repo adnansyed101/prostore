7c04033689619c84c0f504da196f85c5b18c6646
adnansyed101 <sirgeant10@gmail.com>
2025-05-28 11:57:04 +0600

    Show Loader on adding item or removing item.

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,36 +2,44 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader } from "lucide-react";
 import { toast } from "sonner";
 import { CartItem, Cart } from "@/types";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.action";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddtoCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast("Something went wrong.", {
-        description: res.message,
+      if (!res.success) {
+        toast("Something went wrong.", {
+          description: res.message,
+        });
+        return;
+      }
+
+      // Handle success add to cart
+      toast(res.message, {
+        action: { label: "Go to Cart", onClick: () => router.push("/cart") },
       });
-      return;
-    }
-
-    // Handle success add to cart
-    toast(res.message, {
-      action: { label: "Go to Cart", onClick: () => router.push("/cart") },
     });
   };
 
   // Handle Remove From Cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    toast("Item Removed from cart.", {
-      description: res.message,
+      toast("Item Removed from cart.", {
+        description: res.message,
+      });
+
+      return;
     });
   };
 
@@ -42,16 +50,29 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant={"outline"} onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="h-4 w-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant={"outline"} onClick={handleAddtoCart}>
-        <Plus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="h-4 w-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handleAddtoCart}>
-      <Plus /> AddToCart
+      {isPending ? (
+        <Loader className="h-4 w-4 animate-spin" />
+      ) : (
+        <Plus className="h-4 w-4" />
+      )}{" "}
+      Add to Cart
     </Button>
   );
 };
