b3de4663ee98fae3bb9e8b5933a40f9bf9f5923e
adnansyed101 <sirgeant10@gmail.com>
2025-06-18 11:16:16 +0600

    Create place order form.

---
+++ app/(root)/place-order/place-order-form.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Check, Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useFormStatus } from "react-dom";
+import { createOrder } from "@/lib/actions/order.action";
+
+const PlaceOrderForm = () => {
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const res = await createOrder();
+
+    console.log(res);
+
+    if (res.redirectTo) {
+      return router.push(res.redirectTo);
+    }
+  };
+
+  const PlaceOrderButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending}>
+        {pending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Check className="w-4 h-4" />
+        )}{" "}
+        Place Order
+      </Button>
+    );
+  };
+
+  return (
+    <form className="w-full" onSubmit={handleSubmit}>
+      <PlaceOrderButton />
+    </form>
+  );
+};
+
+export default PlaceOrderForm;
