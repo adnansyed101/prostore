a46990e06fe9ecf1f96efc85dedeed8c77804256
adnansyed101 <sirgeant10@gmail.com>
2025-07-15 12:52:57 +0600

    Add charts component and fix col span number.

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -13,6 +13,7 @@ import { formatCurrency, formatDateTime, formatNumber } from "@/lib/utils";
 import { BadgeDollarSign, Barcode, CreditCardIcon, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import Charts from "./charts";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -30,7 +31,7 @@ const AdminOverviewPage = async () => {
   return (
     <div className="space-y-2">
       <h1 className="h2-bold">Dashboard</h1>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-colos-4">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-0">
             <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
@@ -84,9 +85,15 @@ const AdminOverviewPage = async () => {
           <CardHeader>
             <CardTitle>Overview</CardTitle>
           </CardHeader>
-          <CardContent>{/* Chart here */}</CardContent>
+          <CardContent>
+            <Charts
+              data={{
+                salesData: summary.salesData,
+              }}
+            />
+          </CardContent>
         </Card>
-        <Card className="col-span-4">
+        <Card className="col-span-3">
           <CardHeader>
             <CardTitle>Recent Sales</CardTitle>
           </CardHeader>
