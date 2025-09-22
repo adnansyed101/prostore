4cf5123ffffd96fd344bef7715f07a9f52733a33
adnansyed101 <sirgeant10@gmail.com>
2025-09-22 15:23:31 +0600

    Create onSubmit function to submit form data.

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -5,7 +5,7 @@ import { insertProductSchema, updateProductSchema } from "@/lib/validators";
 import { Product } from "@/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { ControllerRenderProps, useForm } from "react-hook-form";
+import { ControllerRenderProps, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 import {
@@ -20,6 +20,7 @@ import slugify from "slugify";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+import { createProduct, updateProduct } from "@/lib/actions/product.actions";
 
 const ProductForm = ({
   type,
@@ -40,9 +41,55 @@ const ProductForm = ({
       product && type === "Update" ? product : productDefaultValues,
   });
 
+  const onSubmit: SubmitHandler<z.infer<typeof insertProductSchema>> = async (
+    values
+  ) => {
+    if (type === "Create") {
+      const res = await createProduct(values);
+
+      if (!res.success) {
+        toast("Something went wrong.", {
+          description: res.message,
+        });
+      } else {
+        toast("Success", {
+          description: res.message,
+        });
+
+        router.push("/admin/products");
+      }
+    }
+
+    // On Update
+    if (type === "Update") {
+      if (!productId) {
+        router.push("/admin/products");
+        return;
+      }
+
+      const res = await updateProduct({ ...values, id: productId });
+
+      if (!res.success) {
+        toast("Something went wrong.", {
+          description: res.message,
+        });
+      } else {
+        toast("Success", {
+          description: res.message,
+        });
+
+        router.push("/admin/products");
+      }
+    }
+  };
+
   return (
     <Form {...form}>
-      <form action="" className="space-y-8">
+      <form
+        method="POST"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
         <div className="flex flex-col items-start md:flex-row gap-5">
           {/* Name */}
           <FormField
