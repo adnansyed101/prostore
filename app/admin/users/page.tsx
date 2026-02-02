6dfa3d808e756983ecc60da414b884192ce9ef9d
adnansyed101 <sirgeant10@gmail.com>
2026-02-02 14:04:25 +0600

    Create page to list all users.

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,72 @@
+import { getAllUsers } from "@/lib/actions/user.actions";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Metadata } from "next";
+import { formatId } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Pagination from "@/components/pagination";
+import { Badge } from "@/components/ui/badge";
+
+export const metadata: Metadata = {
+  title: "Admin Users",
+};
+
+const AdminUserPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const { page = "1" } = await props.searchParams;
+  const users = await getAllUsers({ page: Number(page) });
+  // console.log(users);
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Users</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Role</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.data.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{formatId(user.id)}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>
+                  {user.role === "user" ? (
+                    <Badge variant="secondary">User</Badge>
+                  ) : (
+                    <Badge variant="default">Admin</Badge>
+                  )}
+                </TableCell>
+                <TableCell>
+                  <Button variant="outline" size="sm" asChild>
+                    <Link href={`/admin/users/${user.id}`}>Edit</Link>
+                  </Button>
+                  {/* <DeleteDialog id={user.id} action={deleteuser} /> */}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {users.totalPages > 1 && (
+          <Pagination page={Number(page) || 1} totalPages={users?.totalPages} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AdminUserPage;
