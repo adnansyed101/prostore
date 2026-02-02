1b98560700415a0037510e8d8c502c5620d201de
adnansyed101 <sirgeant10@gmail.com>
2026-02-02 16:11:31 +0600

    Remove unused import.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,7 +2,6 @@ import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import MainNav from "./main-nav";
-import { Input } from "@/components/ui/input";
 import Menu from "@/components/shared/header/menu";
 import AdminSearch from "@/components/admin/admin-search";
 
