f97581e2e9eedbc294d7564ffad7c4e75ec6af77
adnansyed101 <sirgeant10@gmail.com>
2026-02-15 12:35:56 +0600

    Page to search all products.

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,45 @@
+import ProductCard from "@/components/shared/product/product-card";
+import { getAllProducts } from "@/lib/actions/product.actions";
+
+const SearchPage = async (props: {
+  searchParams: Promise<{
+    q?: string;
+    category?: string;
+    price?: string;
+    rating?: string;
+    sort?: string;
+    page?: string;
+  }>;
+}) => {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+    sort = "newest",
+    page = "1",
+  } = await props.searchParams;
+  const products = await getAllProducts({
+    query: q,
+    category,
+    price,
+    rating,
+    sort,
+    page: Number(page),
+  });
+  return (
+    <div className="grid md:grid-cols-5 md:gap-5">
+      <div className="filter-links">{/* Filter Links */}</div>
+      <div className="md:col-span-4 space-y-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {products.data.length === 0 && <p>No Products Found</p>}
+          {products.data.map((x) => (
+            <ProductCard key={x.id} product={x} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
