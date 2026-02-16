89addc4902ccebce09630e7e308e6abf2f0dbe1c
adnansyed101 <sirgeant10@gmail.com>
2026-02-16 15:00:57 +0600

    Create Reveiw Schema.

--- lib/validators.ts
+++ lib/validators.ts
@@ -128,3 +128,16 @@ export const updateUserSchema = updateProfileSchema.extend({
   id: z.string().min(1, "ID is required"),
   role: z.string().min(1, "Role is required"),
 });
+
+// Schema to insert reviews
+export const insertReviewSchema = z.object({
+  title: z.string().min(3, "Title must be at least 3 characters"),
+  description: z.string().min(3, "Comment must be at least 3 characters"),
+  productId: z.string().min(1, "Product ID is required"),
+  userId: z.string().min(1, "User ID is required"),
+  rating: z.coerce
+    .number()
+    .int()
+    .min(1, "Rating must be at least 1")
+    .max(5, "Rating must be at most 5"),
+});
